87c05c8a8ecb801cd9a2384e6a7691455165b839
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-19 10:55:31 +0900

    create ast struct

---
+++ 01/src/monkey/ast/ast.go
@@ -0,0 +1,18 @@
+import "monkey/token"
+
+type LetStatement struct {
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }

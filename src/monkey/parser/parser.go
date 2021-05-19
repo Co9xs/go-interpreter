247c6279dcd0117d7c1ab540b2846fd112e29233
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-19 14:36:11 +0900

    add test of ExpressionStatement

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -7,11 +7,18 @@ import (
 	"fmt"
 )
 
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn func(ast.Expression) ast.Expression
+)
+
 type Parser struct {
 	l *lexer.Lexer
 	curToken token.Token
 	peekToken token.Token
 	errors []string
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -111,3 +118,12 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		return false
 	}
 }
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
+}
+

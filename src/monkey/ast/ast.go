9cc847ce925588fa058e35637cac9d00f2548221
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-22 11:34:57 +0900

    add functionLiteral ast

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"monkey/token"
 	"bytes"
+	"strings"
 )
 
 type Node interface {
@@ -198,3 +199,28 @@ type Boolean struct {
 func (b *Boolean) expressionNode() {}
 func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
 func (b *Boolean) String() string { return b.Token.Literal }
+
+type FunctionLiteral struct {
+	Token token.Token // 'fn'トークン
+	Parameters []*Identifier
+	Body *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+}

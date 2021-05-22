9c0d754efbef9e6162603fe268630a7d9408f948
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-22 12:55:52 +0900

    expand statement test case

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -129,16 +129,20 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	// TODO: セミコロンまで読み飛ばしているので要修正
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+	stmt.Value = p.parseExpression(LOWEST)
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
+
 	return stmt
 }
 
 func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	stmt := &ast.ReturnStatement{Token: p.curToken}
 	p.nextToken()
+
+	stmt.ReturnValue = p.parseExpression(LOWEST)
 
 	// TODO: セミコロンまで読み飛ばしているので要修正
 	for !p.curTokenIs(token.SEMICOLON) {

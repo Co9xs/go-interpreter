b8f77da2c494e566773ab745ddfdb16bb017c839
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-26 12:41:34 +0900

    expand evaluator

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -14,11 +14,11 @@ var (
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
-		return evalProgram(node.Statements)
+		return evalProgram(node)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	case *ast.BlockStatement:
-		return evalBlockStatement(node.Statements)
+		return evalBlockStatement(node)
 	case *ast.IfExpression:
 		return evalIfExpression(node)
 	case *ast.PrefixExpression:
@@ -39,10 +39,9 @@ func Eval(node ast.Node) object.Object {
 	return nil
 }
 
-
 func evalProgram(program *ast.Program) object.Object {
 	var result object.Object
-	
+
 	for _, statement := range program.Statements {
 		result = Eval(statement)
 		if returnValue, ok := result.(*object.ReturnValue); ok {
@@ -56,15 +55,26 @@ func evalBlockStatement(block *ast.BlockStatement) object.Object {
 	var result object.Object
 
 	for _, statement := range block.Statements {
-		result := Eval(statement)
+		result = Eval(statement)
 		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
 			return result
 		}
 	}
-
 	return result
 }
- 
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	
+	for _, statement := range stmts {
+		result = Eval(statement)
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
+	}
+	return result
+}
+
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	if input {
 		return TRUE

c900adbfc0be3b1acd77bcbc5836bced7adcc9d3
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-31 18:15:39 +0900

    add hash literal ast

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -296,3 +296,24 @@ func (fl *FunctionLiteral) String() string {
 
 	return out.String()
 }
+
+type HashLiteral struct {
+	Token token.Token
+	Pairs map[Expression]Expression
+}
+
+func (hl *HashLiteral) expressionNode() {}
+func (hl *HashLiteral) TokenLiteral() string { return hl.Token.Literal }
+func (hl *HashLiteral) String() string {
+	var out bytes.Buffer
+	pairs := []string{}
+	for key, value := range hl.pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+
+		out.WriteString("{")
+		out.WriteString(strings.Join(pairs, ", "))
+		out.WriteString("}")
+
+		return out.String()
+	}
+}

6584c6ba45e4437b5d0654a5b29ab662f3a40221
Co9xs <ryo.fjsm0203@icloud.com>
2021-05-31 18:26:31 +0900

    add parse function for hash literal

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -307,13 +307,11 @@ func (hl *HashLiteral) TokenLiteral() string { return hl.Token.Literal }
 func (hl *HashLiteral) String() string {
 	var out bytes.Buffer
 	pairs := []string{}
-	for key, value := range hl.pairs {
+	for key, value := range hl.Pairs {
 		pairs = append(pairs, key.String()+":"+value.String())
-
+	}
 		out.WriteString("{")
 		out.WriteString(strings.Join(pairs, ", "))
 		out.WriteString("}")
-
 		return out.String()
-	}
 }
